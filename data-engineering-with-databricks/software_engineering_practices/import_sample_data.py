c7b263c7f2f37e1ac3a47264f11f33af58363675
kai-at-hallinc <kai-at-hallinc>
2025-08-25 11:27:03 +0000

    course complete

---
+++ data-engineering-with-databricks/software_engineering_practices/import_sample_data.py
@@ -0,0 +1,3 @@
+# Databricks notebook source
+# DBTITLE 1,install python wheel
+# MAGIC %pip install /Workspace/Users/adm-kai.hall@finavia.fi/databricks_data_engineering/data-engineering-with-databricks/software_engineering_practices/data_setup-0.0.5-py3-none-any.whl
